39c775cc9b6a6059eaee9c2f06c1f755f9c0b136
harshakantha94 <harshakantha2020@gmail.com>
2016-10-02 15:22:47 +0530

    Edit Product

---
+++ SLA_Staff/Controllers/productsController.cs
@@ -0,0 +1,107 @@
+﻿using SLA_Staff.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SLA_Staff.Controllers
+{
+    public class productsController : Controller
+    {
+        //SqlConnection conn = new SqlConnection("Data Source=DESKTOP-LA58UBF;Initial Catalog=hackathon;Integrated Security=SSPI");
+
+        //GET: products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: products/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: products/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: products/Edit/5
+        public ActionResult Edit(int id)
+        {
+
+            return View();
+        }
+
+        // POST: products/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                string i="1";
+                //conn.open();
+                ProductEdit product = new ProductEdit();
+                product.ID1 =Convert.ToInt32(Request.Form["txtid"]);
+                product.Name1 = Request.Form["txtname"];
+                product.Price = Convert.ToInt32(Request.Form["txtprice"]);
+                product.Qty = Convert.ToInt32(Request.Form["txtqty"]);
+
+                string sql = "update products set productId='"+product.ID1+"',name='"+product.Name1+"',qty='"+product.Qty+"',price='"+product.Price+"'";
+                return RedirectToAction("EditProduct");
+
+                //SqlCommand cmd = new SqlCommand(sql, conn);
+                //cmd.ExecuteNonQuery();
+                //conn.Close();
+                //conn.Dispose();
+            }
+            catch
+            {
+
+                return View();
+            }
+        }
+
+        // GET: products/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: products/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SLA_Staff/Models/ProductEdit.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLA_Staff.Models
+{
+    public class ProductEdit
+    {
+        int ID;
+        string Name;
+        int qty;
+        int price;
+
+        public int ID1
+        {
+            get
+            {
+                return ID;
+            }
+
+            set
+            {
+                ID = value;
+            }
+        }
+
+        public string Name1
+        {
+            get
+            {
+                return Name;
+            }
+
+            set
+            {
+                Name = value;
+            }
+        }
+
+        public int Qty
+        {
+            get
+            {
+                return qty;
+            }
+
+            set
+            {
+                qty = value;
+            }
+        }
+
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+
+
+       
+    }
+}

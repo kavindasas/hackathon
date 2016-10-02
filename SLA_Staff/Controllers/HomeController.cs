ecf8ee5463afcaef72ca52e125f25e23db63de29
kavindasas <kaviliya123@gmail.com>
2016-10-02 11:06:55 +0530

    Updated Login
    Login
    SignUp

--- SLA_Staff/Controllers/HomeController.cs
+++ SLA_Staff/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +9,71 @@ namespace SLA_Staff.Controllers
 {
     public class HomeController : Controller
     {
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-BQ663OC;Initial Catalog=StaffDB;Integrated Security=SSPI");
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult login()
+        {
+            try
+            {
+                conn.Open();
+                string uname = Request.Form["name"];
+                string password = Request.Form["password"];
+
+                SqlCommand cmd = new SqlCommand("select * from user where username='" + uname + "' and password= '" + password + "'", conn);
+                SqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    Session["user"] = uname;
+                    return RedirectToAction("Home");
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+                conn.Close();
+                conn.Dispose();
+
+
+                
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpPost]
+        public ActionResult signup()
+        {
+            try
+            {
+                conn.Open();
+                string username = Request.Form["uname"];
+                string fname = Request.Form["fname"];
+                string lname = Request.Form["lname"];
+                string email = Request.Form["email"];
+                string password = Request.Form["password"];
+
+                SqlCommand cmd = new SqlCommand("insert into user(username,fname,lname,email,password) values('"+username+ "','" + fname + "','" + lname + "','" + email + "','" + password + "')", conn);
+                cmd.ExecuteNonQuery();
+               
+                Session["user"] = username;
+                return RedirectToAction("Home");
+                conn.Close();
+                conn.Dispose();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
         }
 
         public ActionResult About()
@@ -20,10 +83,9 @@ namespace SLA_Staff.Controllers
             return View();
         }
 
-        //careersSample
-        public ActionResult careersSample()
+        public ActionResult Home()
         {
-            ViewBag.Message = "Your application description page.";
+            
 
             return View();
         }
